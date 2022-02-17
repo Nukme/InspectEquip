64105d9a87ea038d312cc1d076afb5202d68107d
CCJ <ccj1988@gmail.com>
2022-02-17 12:10:33 +0800

    init

---
+++ LocalDatabase.lua
@@ -0,0 +1,643 @@
+if not InspectEquip then return end
+
+local IE = InspectEquip
+local IS = InspectEquip_ItemSources
+local L = LibStub("AceLocale-3.0"):GetLocale("InspectEquip")
+
+local band = bit.band
+local bor = bit.bor
+local tinsert = table.insert
+local strfind = string.find
+local max = math.max
+
+-- check for 5.0+ client, because EJ api was changed
+local leg = select(4, GetBuildInfo()) >= 70000
+
+-- Database
+InspectEquipLocalDB = {}
+local ieVersion
+
+-- Max wait cycles for database update
+local MAX_WC = 1 --5
+local DATA_RECEIVED_WC = 1 --7
+
+local newDataReceived
+local newDataCycles = 0
+local dbInitialized = false
+
+-- GUI
+local bar, barText
+local coUpdate
+local bar2
+
+function IE:InitLocalDatabase()
+	if dbInitialized then return end
+
+	if not InspectEquipLocalDB then InspectEquipLocalDB = {} end
+	setmetatable(InspectEquipLocalDB, {__index = {
+		Zones = {}, Bosses = {}, Items = {}
+	}})
+
+	local _, currentBuild = GetBuildInfo()
+	ieVersion = GetAddOnMetadata("InspectEquip", "Version")
+
+	-- create database if not present or outdated (or wrong locale)
+	if (InspectEquipLocalDB.ClientBuild ~= currentBuild) or
+			(InspectEquipLocalDB.Locale ~= GetLocale()) or
+			(InspectEquipLocalDB.IEVersion ~= ieVersion) or
+			(InspectEquipLocalDB.Expansion ~= GetExpansionLevel()) then
+		--self:CreateLocalDatabase()
+		self:ScheduleTimer("CreateLocalDatabase", 5)
+	end
+	dbInitialized = true
+end
+
+local function createUpdateGUI()
+
+	if not bar then
+		bar = CreateFrame("STATUSBAR", nil, UIParent, "TextStatusBar, BackdropTemplate")
+	end
+	bar:SetWidth(300)
+	bar:SetHeight(30)
+	bar:SetPoint("CENTER", 0, -100)
+	bar:SetBackdrop({
+		bgFile	 = "Interface\\Tooltips\\UI-Tooltip-Background",
+		edgeFile = "Interface\\Tooltips\\UI-Tooltip-Border",
+		tile = 1, tileSize = 10, edgeSize = 10,
+		insets = {left = 1, right = 1, top = 1, bottom = 1}
+	})
+	bar:SetStatusBarTexture("Interface\\TargetingFrame\\UI-StatusBar")
+	bar:SetBackdropColor(0, 0, 0, 1)
+	bar:SetStatusBarColor(0.6, 0.6, 0, 0.4)
+	bar:SetMinMaxValues(0, 100)
+	bar:SetValue(0)
+	bar:Hide()
+	
+	if not bar2 then
+		bar2 = CreateFrame("STATUSBAR", nil, UIParent, "TextStatusBar, BackdropTemplate")
+	end
+	bar2:SetWidth(300)
+	bar2:SetHeight(10)
+	bar2:SetPoint("CENTER", 0, -130)
+	bar2:SetBackdrop({
+		bgFile	 = "Interface\\Tooltips\\UI-Tooltip-Background",
+		edgeFile = "Interface\\Tooltips\\UI-Tooltip-Border",
+		tile = 1, tileSize = 10, edgeSize = 10,
+		insets = {left = 1, right = 1, top = 1, bottom = 1}
+	})
+	bar2:SetStatusBarTexture("Interface\\TargetingFrame\\UI-StatusBar")
+	bar2:SetBackdropColor(0, 0, 0, 1)
+	bar2:SetStatusBarColor(0, 1, 1, 0.6 )
+	bar2:SetMinMaxValues(0, 100)
+	bar2:SetValue(0)
+	bar2:Hide()
+
+	if not barText then
+		barText = bar:CreateFontString(nil, "OVERLAY", "GameFontHighlight")
+	end
+	barText:SetPoint("CENTER", bar, "CENTER")
+	barText:SetJustifyH("CENTER")
+	barText:SetJustifyV("CENTER")
+	barText:SetTextColor(1, 1, 1)
+	barText:SetText("InspectEquip: " .. L["Updating database..."])
+
+end
+
+local ejDisabled = false
+--local ejToggle = ToggleEncounterJournal
+
+-- this function disables the EJ GUI during update
+-- because using the EJ during update will result in an invalid database
+-- (other addons may still use the EJ api and possibly destroy the DB that way...)
+local function DisableEJ()
+	if ejDisabled then return end
+	ejDisabled = true
+
+	-- hide EJ
+	if EncounterJournal then
+		if EncounterJournal:IsShown() then
+			EncounterJournal:Hide()
+		end
+	end
+
+	-- 2020/12/4 note: ToggleEncounterJournal() is a protected function, so
+	-- any attempt to replace it or other functions within it would cause taint
+	-- and result in block message & UI lock-in-combat after scanning EJ to build database.
+	-- A /reload could restore the protected state of ToggleEncounterJournal().
+	-- Use Ace Hook as a workaround.
+	-- Idea is to enforce the hiding of EJ UI.
+
+	--ejToggle = ToggleEncounterJournal
+	--ToggleEncounterJournal = function() end
+	IE:SecureHook("ToggleEncounterJournal", function() EncounterJournal:Hide() end)
+	EJMicroButton:Disable()
+end
+
+local function EnableEJ()
+	if not ejDisabled then return end
+	ejDisabled = false
+
+	--ToggleEncounterJournal = ejToggle
+	IE:Unhook("ToggleEncounterJournal")
+	EJMicroButton:Enable()
+	EJMicroButton:SetAlpha(1)
+end
+
+local function EndUpdate()
+	bar:SetScript("OnUpdate", nil)
+	bar:Hide()
+	bar2:Hide()
+	EnableEJ()
+	coUpdate = nil
+	IE:UnregisterEvent("EJ_LOOT_DATA_RECIEVED")
+end
+
+local function UpdateTick()
+	if coUpdate then
+		-- wait for loot data
+		if newDataReceived then -- data received this frame
+			newDataCycles = newDataCycles + DATA_RECEIVED_WC
+			newDataReceived = false
+		end
+		if newDataCycles > 0 then
+			newDataCycles = newDataCycles - 1
+			return
+		end
+		local ok, msg = coroutine.resume(coUpdate)
+		if not ok then
+			EndUpdate()
+			message("[InspectEquip] Could not update database: " .. msg)
+		end
+	end
+end
+
+local function UpdateBar()
+	bar:SetValue(bar:GetValue() + 1)
+	coroutine.yield()
+end
+
+local function UpdateBar2()
+	bar2:SetValue(bar2:GetValue()+1)
+	coroutine.yield()
+end
+
+local function GetReverseMapping(baseMap)
+	local map = {}
+	local id, name
+	for id, name in pairs(baseMap) do
+		map[name] = id
+	end
+	return map
+end
+
+local function DifficultyToMode(diff, raid)
+	if diff == 1 then
+		-- 5 normal
+		return 1
+	elseif diff == 2 then
+		-- 5 heroic
+		return 2
+	elseif diff == 23 then
+		-- 5 mythic(dungeons)
+		return 4
+	elseif diff == 24 then
+		-- 5 timewalker
+		return 8
+	elseif diff == 3 then
+		-- 10 normal
+		return 16
+	elseif diff == 4 then
+		-- 25 normal
+		return 32
+	elseif diff == 5 then
+		-- 10 heroic
+		return 64
+	elseif diff == 6 then
+		-- 25 heroic
+		return 128
+	elseif diff == 7 then
+		-- 25 lfr  (Legacy LFRs; everything prior to Siege of Orgrimmar)
+		return 256
+	elseif diff == 9 then
+		-- 40 vanilla raid
+		return 512
+	elseif diff == 14 then
+		-- normal(raids)
+		return 1024
+	elseif diff == 15 then
+		-- heroic(raids)
+		return 2048
+	elseif diff == 16 then
+		-- mythic(raids)
+		return 4096
+	elseif diff == 17 then
+		-- lfr(raids)
+		return 8192
+	elseif diff == 33 then
+		-- raid timewalker
+		return 16384
+	end
+end
+
+local function AddToDB(tempDB, itemID, zoneID, bossID, mode)
+	local sources = tempDB[itemID]
+	local entry
+	if sources then
+		for _, entry in pairs(sources) do
+			if (entry[1] == zoneID) and (entry[2] == bossID) then
+				entry[3] = bor(entry[3], mode)
+				return
+			end
+		end
+		tinsert(sources, {zoneID, bossID, mode})
+	else
+		tempDB[itemID] = {{zoneID, bossID, mode}}
+	end
+end
+
+local function SaveToDB(tempDB, entryType)
+	local itemID, sources, entry
+	for itemID, sources in pairs(tempDB) do
+		local str = InspectEquipLocalDB.Items[itemID]
+		local isEntry = IS.Items[itemID]
+
+		-- loop through sources we found
+		for _, entry in pairs(sources) do
+			local entryStr = entryType .. "_" .. entry[1] .. "_" .. entry[3] .. "_" .. entry[2]
+
+			-- skip if already in IS DB
+			if not (isEntry and (strfind(";" .. isEntry .. ";", ";" .. entryStr .. ";"))) then
+				if str then
+					str = str .. ";" .. entryStr
+				else
+					str = entryStr
+				end
+			end
+
+		end
+
+		InspectEquipLocalDB.Items[itemID] = str
+	end
+end
+
+local function GetInstanceCount(isRaid)
+	local i = 1
+	local id
+	repeat
+		id = EJ_GetInstanceByIndex(i, isRaid)
+		i = i + 1
+	until not id
+	return i - 2
+end
+
+local function GetTotalLootCount()
+	bar2:Show()
+	local lootCount = 0
+	local tierCount = EJ_GetNumTiers()
+	local tier, i
+	for tier = 1, tierCount do
+		if leg then
+			EJ_SelectTier(tier)
+		end
+
+		local isRaid = false
+		while true do
+			i = 1
+			local insID, insName = EJ_GetInstanceByIndex(i, isRaid)
+
+			while insID do
+				EJ_SelectInstance(insID)
+				local diff
+				local difficulties
+				if leg then
+					if isRaid then
+						difficulties = {3, 4, 5, 6, 7, 9, 14, 15, 16, 17, 33}
+					else
+						difficulties = {1, 2, 23, 24}
+					end
+				end
+				for _, diff in ipairs(difficulties) do
+					if EJ_IsValidInstanceDifficulty(diff) then
+						EJ_SetDifficulty(diff)
+						lootCount = lootCount + EJ_GetNumLoot()
+					elseif (insID == 741 or insID == 742 or insID == 744) and diff == 9 then
+						EJ_SetDifficulty(diff)
+						lootCount = lootCount + EJ_GetNumLoot()
+					end
+				end
+				UpdateBar2()
+				i = i + 1
+				insID, insName = EJ_GetInstanceByIndex(i, isRaid)
+			end
+			
+			if isRaid then
+				break
+			else
+				isRaid = true
+			end
+		end
+	end
+	return lootCount
+end
+
+local function UpdateFunction(recursive)
+	local _, i, j, tier
+
+	-- reset EJ
+	DisableEJ()
+	EJ_EndSearch()
+	EJ_ClearSearch()
+	if leg then
+		EJ_ResetLootFilter()
+		C_EncounterJournal.ResetSlotFilter()
+	else
+		EJ_SetClassLootFilter(0)
+	end
+	newDataReceived = false
+	IE:RegisterEvent("EJ_LOOT_DATA_RECIEVED")
+
+	-- init/reset database
+	local db = InspectEquipLocalDB
+	db.Zones = {}
+	db.Bosses = {}
+	db.Items = {}
+	db.NextZoneID = 1000
+	db.NextBossID = 1000
+
+	-- count total number of instances
+	local insCount = 0
+	local tierCount = 1
+	if leg then
+		-- as of 5.x there are multiple tiers (classic, bc, wotlk, cata, mop...)
+		tierCount = EJ_GetNumTiers()
+		for tier = 1, tierCount do
+			local tierName = EJ_GetTierInfo(tier)
+			EJ_SelectTier(tier)
+			--IE:Print("Tier: " .. tostring(tier) .. " = " .. tierName)
+			local dungeonCount = GetInstanceCount(false)
+			local raidCount = GetInstanceCount(true)
+			--IE:Print(" ==> " .. tostring(dungeonCount) .. " Dungeons, " .. tostring(raidCount) .. " Raids")
+			insCount = insCount + dungeonCount + raidCount
+		end
+	else
+		local dungeonCount = GetInstanceCount(false)
+		local raidCount = GetInstanceCount(true)
+		insCount = dungeonCount + raidCount
+		--IE:Print(" ==> " .. tostring(dungeonCount) .. " Dungeons, " .. tostring(raidCount) .. " Raids")
+	end
+
+	-- set bar max value
+	local startValue = bar:GetValue()
+	bar:SetMinMaxValues(0, startValue + insCount + 2)
+	bar2:SetMinMaxValues(0, startValue + insCount)
+
+	-- get IS mapping for zone/boss name -> zone/boss id
+	local zoneMap = GetReverseMapping(IS.Zones)
+	local bossMap = GetReverseMapping(IS.Bosses)
+
+	UpdateBar()
+
+	-- temp db to allow for merging of modes
+	local tempDungeonDB = {}
+	local tempRaidDB = {}
+	local totalLootCount = 0
+
+	local waitCycles = 0
+	-- get loot
+	for tier = 1, tierCount do
+		if leg then
+			EJ_SelectTier(tier)
+		end
+
+		-- loop through all instances of this tier, dungeons first, then raids
+		local isRaid = false
+		while true do
+			i = 1
+			local insID, insName = EJ_GetInstanceByIndex(i, isRaid)
+			local tempDB = isRaid and tempRaidDB or tempDungeonDB
+
+			while insID do
+				-- get zone id for db
+				local zoneID = zoneMap[insName]
+				if not zoneID then
+					-- zone is not in db
+					--zoneID = db.NextZoneID
+					zoneID = insID
+					db.Zones[zoneID] = insName
+					zoneMap[insName] = zoneID
+					--db.NextZoneID = db.NextZoneID + 1
+				end
+
+				EJ_SelectInstance(insID)
+				local diff
+				local difficulties
+				if leg then
+					if isRaid then
+						difficulties = {3, 4, 5, 6, 7, 9, 14, 15, 16, 17, 33}
+					else
+						difficulties = {1, 2, 23, 24}
+					end
+				end
+				for _, diff in ipairs(difficulties) do
+					if EJ_IsValidInstanceDifficulty(diff) then
+						newDataCycles = DATA_RECEIVED_WC * 3
+						EJ_SetDifficulty(diff)
+						local mode = DifficultyToMode(diff, isRaid)
+						local n = EJ_GetNumLoot()
+
+						-- the problem here is that when the items are not in the cache, EJ_GetNumLoot() will not
+						-- return the correct number. it returns the number of items that are cached, which may be
+						-- 0 or a number lower than the correct number. it seems to be impossible to determine the
+						-- correct number without waiting. EJ_LOOT_DATA_RECIEVED events are received, but we don't
+						-- know which event is the last one. also, no EJ_LOOT_DATA_RECIEVED are received if all
+						-- items are already in the cache. so we try to wait a couple of frames until we have a
+						-- number that is somehow stable.
+						-- at the end we count all items again, and if we then have more items, we start all over
+						-- again (at this point, the items are already cached, so the 2nd iteration is faster).
+						
+						-- Not sure but it seems that Blizzard has made EJ pre-loaded and the loot count is stable in LEG.
+						
+						local wc = 0
+						local wcMax = MAX_WC
+						while wc < wcMax do
+							coroutine.yield()
+							wc = wc + 1
+							local nNew = EJ_GetNumLoot()
+							if n ~= nNew then
+								wcMax = wc + MAX_WC
+							end
+							n = nNew
+						end
+						waitCycles = waitCycles + wc
+
+						totalLootCount = totalLootCount + n
+
+						--IE:Print("T=" .. tostring(tier) .. " I=" .. insName .. " D=" .. diff .. " M=" .. mode .. " ! #loot = " .. n .. " [wc = " .. wc .. "]")
+
+						for j = 1, n do
+							-- get item info
+							--local itemID, encID, itemName, _, _, _, itemLink = C_EncounterJournal.GetLootInfoByIndex(j)
+							local t = C_EncounterJournal.GetLootInfoByIndex(j)
+							local itemID = t.itemID
+							local encID = t.encounterID
+							local itemName = t.name
+							local itemLink = t.link
+
+							-- wait until data has arrived
+							-- this doesn't seem necessary
+							-- while not itemID do
+							--	coroutine.yield()
+							--	waitCycles = waitCycles + 1
+							--	itemName, _, _, _, itemID, itemLink, encID = EJ_GetLootInfoByIndex(j)
+							-- end
+
+							local encName = EJ_GetEncounterInfo(encID)
+
+							-- if not encName then
+							--	IE:Print("no encounter name! encID = " .. encID)
+							-- end
+
+							-- get boss id for db
+							local bossID = bossMap[encName]
+							if not bossID then
+								-- boss is not in db
+								-- bossID = db.NextBossID
+								bossID = encID -- unique ID of the encounter
+								db.Bosses[bossID] = encName
+								bossMap[encName] = bossID
+								--db.NextBossID = db.NextBossID + 1
+							end
+
+							-- add item to db
+							AddToDB(tempDB, itemID, zoneID, bossID, mode)
+						end
+					elseif (insID == 741 or insID == 742 or insID == 744) and diff == 9 then
+						newDataCycles = DATA_RECEIVED_WC * 3
+						EJ_SetDifficulty(diff)
+						local mode = DifficultyToMode(diff, isRaid)
+						local n = EJ_GetNumLoot()
+						local wc = 0
+						local wcMax = MAX_WC
+						while wc < wcMax do
+							coroutine.yield()
+							wc = wc + 1
+							local nNew = EJ_GetNumLoot()
+							if n ~= nNew then
+								wcMax = wc + MAX_WC
+							end
+							n = nNew
+						end
+						waitCycles = waitCycles + wc
+
+						totalLootCount = totalLootCount + n
+						for j = 1, n do
+							-- get item info
+							--local itemID, encID, itemName, _, _, _, itemLink = EJ_GetLootInfoByIndex(j)
+
+							local t = C_EncounterJournal.GetLootInfoByIndex(j)
+							local itemID = t.itemID
+							local encID = t.encounterID
+							local itemName = t.name
+							local itemLink = t.link
+
+							local encName = EJ_GetEncounterInfo(encID)
+
+							-- get boss id for db
+							local bossID = bossMap[encName]
+							if not bossID then
+								-- boss is not in db
+								-- bossID = db.NextBossID
+								bossID = encID -- unique ID of the encounter
+								db.Bosses[bossID] = encName
+								bossMap[encName] = bossID
+								--db.NextBossID = db.NextBossID + 1
+							end
+
+							-- add item to db
+							AddToDB(tempDB, itemID, zoneID, bossID, mode)
+						end
+					end
+				end
+
+				-- next instance
+				UpdateBar()
+				i = i + 1
+				insID, insName = EJ_GetInstanceByIndex(i, isRaid)
+			end
+
+			if isRaid then
+				-- done with dungeons + raids, next tier
+				break
+			else
+				-- done with dungeons, now do it again for raids
+				isRaid = true
+			end
+		end
+	end
+
+	-- save to db
+	
+	SaveToDB(tempDungeonDB, "d")
+	SaveToDB(tempRaidDB, "r")
+	local _, currentBuild = GetBuildInfo()
+	db.ClientBuild = currentBuild
+	db.Locale = GetLocale()
+	db.IEVersion = ieVersion
+	db.Expansion = GetExpansionLevel()
+	
+	UpdateBar()
+	
+	-- Check loot count
+	local lootCountVerification = GetTotalLootCount()
+
+	--IE:Print("totalLootCount = " .. totalLootCount .. " / lootCountVerification = " .. lootCountVerification)
+	
+	-- Check if EJ db is stable
+	if totalLootCount < lootCountVerification then
+		-- We missed some items, retry...
+		--IE:Print("Restarting update...")
+		UpdateFunction((recursive and recursive or 0) + 1)
+	else
+		-- Done
+		EndUpdate()
+		--IE:Print("Wait cycles = " .. waitCycles)
+	end
+	
+end
+
+function IE:EJ_LOOT_DATA_RECIEVED(event, itemID)
+	-- self:Print("LOOT_DATA_RECEIVED [" .. (itemID and itemID or "nil") .. "]")
+	newDataReceived = true
+end
+
+function IE:CreateLocalDatabase()
+
+	if coUpdate then
+		-- update already in progress
+		return
+	end
+
+	-- load encounter journal
+	if not IsAddOnLoaded("Blizzard_EncounterJournal") then
+		local loaded, reason = LoadAddOn("Blizzard_EncounterJournal")
+		if not loaded then
+			message("[InspectEquip] Could not load encounter journal: " .. reason)
+		end
+	end
+
+	-- show progress bar
+	createUpdateGUI()
+	bar:Show()
+
+	-- start update
+	coUpdate = coroutine.create(UpdateFunction)
+	local ok, msg = coroutine.resume(coUpdate)
+	if ok then
+		bar:SetScript("OnUpdate", UpdateTick)
+	else
+		EndUpdate()
+		message("[InspectEquip] Could not update database: " .. msg)
+	end
+
+end
+
---
+++ Tooltips.lua
@@ -0,0 +1,339 @@
+if not InspectEquip then return end
+
+local IE = InspectEquip
+local IS = InspectEquip_ItemSources
+local L = LibStub("AceLocale-3.0"):GetLocale("InspectEquip")
+
+local band = bit.band
+local tinsert = table.insert
+local ssub = string.sub
+local strformat = string.format
+
+local addSource, addItemData
+local unknownIcon = "Interface\\ICONS\\INV_Misc_QuestionMark"
+
+local leg = select(4, GetBuildInfo()) >= 70000
+
+addSource = function(tip, item, source, level)
+	local next_field = gmatch(source, "[^_]+")
+
+	local cat = next_field()
+	local str = nil
+	local subItems = {}
+	if cat == "r" or cat == "d" then
+		-- raid/dungeon, drops and quest items
+		local zone = IE:GetZoneName(tonumber(next_field()))
+
+		--if cat == "r" then
+			--str = L["Raid"] .. ": " .. zone
+		--else
+			--str = L["Instances"] .. ": " .. zone
+		--end
+
+		local mode = next_field()
+		if mode == "q" then
+			-- quest reward
+			str = str .. " - " .. L["Quest Reward"]
+		else
+			-- drop
+			mode = tonumber(mode)
+			local boss = IE:GetBossName(tonumber(next_field() or 0))
+			if boss then
+				str = zone .. " - " .. boss
+			end
+			-- mode
+			if cat == "r" then
+				-- raid
+				if mode > 15 then
+					local sum = 0
+					local modebit = mode
+					while modebit ~= 0 do
+						sum = sum + bit.band(modebit,1)
+						modebit = bit.rshift(modebit,1)
+					end
+					if sum == 1 then
+						if mode == 16 then 
+							str = str .. " (" .. PLAYER_DIFFICULTY1 .. " 10)" -- 10 Normal
+						elseif mode == 32 then
+							str = str .. " (" .. PLAYER_DIFFICULTY1 .. " 25)" -- 25 Normal
+						elseif mode == 64 then
+							str = str .. " (" .. PLAYER_DIFFICULTY2 .. " 10)" -- 10 Heroic
+						elseif mode == 128 then
+							str = str .. " (" .. PLAYER_DIFFICULTY2 .. " 25)" -- 25 Heroic
+						elseif mode == 256 then
+							str = str .. " (" .. PLAYER_DIFFICULTY3 .. ")" -- LFR
+						elseif mode == 512 then
+							str = str .. " (" .. PLAYER_DIFFICULTY1 .. " 40)" -- 40 Normal
+						elseif mode == 1024 then
+							str = str .. " (" .. PLAYER_DIFFICULTY1 .. ")" --  Normal
+						elseif mode == 2048 then
+							str = str .. " (" .. PLAYER_DIFFICULTY2 .. ")" --  Heroic
+						elseif mode == 4096 then
+							str = str .. " (" .. PLAYER_DIFFICULTY6 .. ")" -- Mythic
+						elseif mode == 8192 then
+							str = str .. " (" .. PLAYER_DIFFICULTY3 .. ")" -- LFR
+						elseif mode == 16384 then
+							str = str .. " (" .. PLAYER_DIFFICULTY_TIMEWALKER .. ")" -- Timewalker
+						end
+					elseif sum == 2 then
+						local n10 = (band(mode, 16) == 16)
+						local n25 = (band(mode, 32) == 32)
+						local h10 = (band(mode, 64) == 64)
+						local h25 = (band(mode, 128) == 128)
+						local lfr_legacy = (band(mode,256) == 256)
+						local normal_legacy = (band(mode,512) == 512)
+						local normal = (band(mode,1024) == 1024)
+						local heroic = (band(mode,2048) == 2048)
+						local mythic = (band(mode,4096) == 4096)
+						local lfr = (band(mode,8192) == 8192)
+						local timewalker = (band(mode, 16384) == 16384)
+						if not timewalker then
+							local dm = ""
+							if n10 then dm = dm .. ", " .. PLAYER_DIFFICULTY1 .. " 10" end
+							if n25 then dm = dm .. ", " .. PLAYER_DIFFICULTY1 .. " 25" end
+							if h10 then dm = dm .. ", " .. PLAYER_DIFFICULTY2 .. " 10" end
+							if h25 then dm = dm .. ", " .. PLAYER_DIFFICULTY2 .. " 25" end
+							if lfr_legacy then dm = dm .. ", " .. PLAYER_DIFFICULTY3 end 
+							if normal_legacy then dm = dm .. ", " .. PLAYER_DIFFICULTY1.." 40" end
+							if lfr then dm = dm .. ", " .. PLAYER_DIFFICULTY3 end
+							if normal then dm = dm .. ", " .. PLAYER_DIFFICULTY1 end
+							if heroic then dm = dm .. ", " .. PLAYER_DIFFICULTY2 end
+							if mythic then dm = dm .. ", " .. PLAYER_DIFFICULTY6 end
+							str = str .. " (" .. ssub(dm, 3) .. ")"
+						end
+					end
+				end
+			else
+				-- dungeon
+				if mode == 1 then
+					str = str .. " (" .. PLAYER_DIFFICULTY1 .. ")" -- Normal
+				elseif mode == 2 then
+					str = str .. " (" .. PLAYER_DIFFICULTY2 .. ")" -- Heroic
+				elseif mode == 4 then
+					str = str .. " (" .. PLAYER_DIFFICULTY6 .. ")" -- Mythic
+				elseif mode == 8 then
+					str = str .. " (" .. PLAYER_DIFFICULTY_TIMEWALKER .. ")" -- Timewalker
+				end
+			end
+		end
+
+	elseif cat == "v" or cat == "g" then
+		-- vendor item
+		if cat == "v" then
+			str = L["Vendor"]
+		else
+			str = L["Guild Vendor"]
+		end
+		local typ = next_field()
+
+		if typ then
+			str = str .. ": "
+			while typ do
+
+				if typ == "c" then
+					-- currency
+					local currency = tonumber(next_field())
+					-- leg compatible
+					if currency == 390 or currency == 392 then
+						-- mark of honor : 137642
+						local curName = GetItemInfo(137642)
+						local curTexture = select(10, GetItemInfo(137642))
+						str = str .. "|T" .. curTexture .. ":0|t " .. " " .. curName .. " "
+					elseif currency == 395 then
+						local curTexture = GetCoinIcon(10000)
+						str = str .. "|T" .. curTexture .. ":0|t " .. " " .. " "
+					end
+					
+					--[[
+					local cost = tonumber(next_field())
+					local curName, _, curTexture = GetCurrencyInfo(currency)
+					if not leg then
+						curTexture = "Interface\\Icons\\" .. curTexture
+					end
+					str = str .. "|T" .. curTexture .. ":0|t " .. cost .. " " .. curName .. " "
+					--]]
+				elseif typ == "i" then
+					-- item
+					local subItemId = tonumber(next_field())
+					local _, subItemLink, _, _, _, _, _, _, _, subItemTexture = GetItemInfo(subItemId)
+					if not subItemLink then
+						subItemLink = "#" .. subItemId
+					end
+					if not subItemTexture then
+						subItemTexture = unknownIcon
+					end
+					str = str .. "|T" .. subItemTexture .. ":0|t " .. subItemLink .. " "
+					tinsert(subItems, subItemId)
+				elseif typ == "m" then
+					-- money
+					local cost = tonumber(next_field())
+					str = str .. GetCoinTextureString(cost) .. " "
+				end
+
+				typ = next_field()
+			end
+		end
+
+	-- currency shortcuts, currently not used
+	--[[
+	elseif cat == "J" then -- Justice Points
+		return addSource(tip, item, "v_c_395_" .. next_field(), level)
+	elseif cat == "V" then -- Valor Points
+		return addSource(tip, item, "v_c_396_" .. next_field(), level)
+	elseif cat == "H" then -- Honor Points
+		return addSource(tip, item, "v_c_392_" .. next_field(), level)
+	elseif cat == "C" then -- Conquest Points
+		return addSource(tip, item, "v_c_390_" .. next_field(), level)
+	]]--
+
+	elseif cat == "f" then -- Reputation rewards
+		str = L["Reputation rewards"]
+	elseif cat == "m" then -- Darkmoon Faire
+		str = L["Darkmoon Faire"]
+	elseif cat == "w" then -- World drops
+		str = L["World drops"]
+
+	elseif cat == "c" then -- Crafted
+		str = L["Crafted"]
+		local prof = GetSpellInfo(tonumber(next_field() or 0))
+		if prof then
+			str = str .. " - " .. prof
+		end
+
+	elseif cat == "q" then -- Quest Reward
+		str = L["Quest Reward"]
+
+	elseif cat == 'p' then  -- PvP Reward
+		str = L["PvP Reward"]
+	end
+
+	-- add line
+	if str then
+		local label
+		local r,g,b = InspectEquipConfig.ttR, InspectEquipConfig.ttG, InspectEquipConfig.ttB
+		if level == 0 then
+			label = L["Source"] .. ": "
+		else
+			local _, subItemLink, _, _, _, _, _, _, _, subItemTexture = GetItemInfo(item)
+			if subItemTexture then
+				label = "    " .. L["Source"] .. "(|T" .. subItemTexture .. ":0|t):"
+			else
+				label = "    " .. L["Source"] .. "(# " .. item .. "):"
+			end
+		end
+		--tip:AddDoubleLine(label, str, r, g, b, r, g, b)
+		tip:AddLine(label..str, r, g, b)
+	end
+
+	-- add sub item info if available
+	if (#subItems > 0) and (level == 0) then
+		for _, subItem in pairs(subItems) do
+			addItemData(tip, subItem, level + 1)
+		end
+	end
+end
+
+addItemData = function(tip, item, level)
+	-- get source information
+	local data = IE:GetItemData(item)
+
+	if data then
+
+		local sourceCount = 0
+		local skippedSourceCount = 0
+		local maxSourceCount = InspectEquipConfig.maxSourceCount
+
+		for entry in gmatch(data, "[^;]+") do
+			if sourceCount < maxSourceCount then
+				addSource(tip, item, entry, level)
+			else
+				skippedSourceCount = skippedSourceCount + 1
+			end
+			sourceCount = sourceCount + 1
+		end
+
+		if skippedSourceCount > 0 then
+			local r,g,b = InspectEquipConfig.ttR, InspectEquipConfig.ttG, InspectEquipConfig.ttB
+			tip:AddLine(strformat(L["... and %d other sources"], skippedSourceCount), r, g, b)
+		end
+
+	end
+end
+
+function IE:AddToTooltip(tip, itemLink)
+	if InspectEquipConfig.tooltips == false then return end
+
+	-- prevent adding information twice for recipe links
+	if tip.InspectEquipItem == itemLink then return end
+	tip.InspectEquipItem = itemLink
+
+	addItemData(tip, itemLink, 0)
+end
+
+local function clearTip(tooltip)
+	tooltip.InspectEquipItem = nil
+end
+
+local function hookTip(tooltip, method, action)
+	if not tooltip then return end
+	hooksecurefunc(tooltip, method, function(tip, ...)
+		local link, count = action(...)
+		if link then
+			IE:AddToTooltip(tip, link)
+		end
+	end)
+end
+
+local function hookCompareTip(tooltip)
+	if not tooltip then return end
+	hooksecurefunc(tooltip, 'SetHyperlinkCompareItem', function(tip, mainLink)
+		local _, link = tip:GetItem()
+		if link then
+			IE:AddToTooltip(tip, link)
+		end
+	end)
+end
+
+local function hookTipScript(tooltip)
+	if tooltip and tooltip.HookScript then
+		tooltip:HookScript('OnTooltipSetItem', function(tip, ...)
+			local _, link = tip:GetItem()
+			if link and GetItemInfo(link) then
+				IE:AddToTooltip(tip, link)
+			end
+		end)
+		tooltip:HookScript('OnTooltipCleared', clearTip)
+	end
+end
+
+function IE:HookTooltips()
+	if IE.tooltipsHooked then return end
+	IE.tooltipsHooked = true
+
+	hookTipScript(GameTooltip)
+	hookTipScript(ItemRefTooltip)
+
+  if leg then
+    hookTipScript(ShoppingTooltip1)
+    hookTipScript(ShoppingTooltip2)
+  else
+    hookCompareTip(ShoppingTooltip1)
+    hookCompareTip(ShoppingTooltip2)
+    hookCompareTip(ShoppingTooltip3)
+    hookCompareTip(ItemRefShoppingTooltip1)
+    hookCompareTip(ItemRefShoppingTooltip2)
+    hookCompareTip(ItemRefShoppingTooltip3)
+  end
+
+	-- Not really needed, but... :-)
+	if AtlasLootTooltipTEMP then
+		hookTipScript(AtlasLootTooltipTEMP)
+	end
+
+	if LinkWrangler and LinkWrangler.RegisterCallback then
+		LinkWrangler.RegisterCallback("InspectEquip", function(frame,link)
+			IE:AddToTooltip(frame,link)
+		end, "item")
+	end
+end
+
