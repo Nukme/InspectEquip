336092b345fbf51b9d9cc141f7c53d5ea8c74d11
CCJ <ccj1988@gmail.com>
2022-04-24 23:48:12 +0800

    structure update

---
+++ Utilities/data/seasonaffixes.lua
@@ -0,0 +1,21 @@
+local _, _table_ = ...
+
+local L = LibStub("AceLocale-3.0"):GetLocale("InspectEquip")
+
+local seasonAffix = {
+    L["Dread"], -- BFA Season 1
+    L["Sinister"], -- BFA Season 2
+    L["Notorious"], -- BFA Season 3
+    L["Corrupted"], -- BFA Season 4
+    L["Sinful"], -- Shadowlands Season 1
+    L["Unchained"], -- Shadowlands Season 2
+    L["Cosmic"], -- Shadowlands Season 3
+}
+
+local subAffix = {
+    L["Aspirant"], 
+    L["Gladiator"],
+}
+
+_table_.seasonAffix = seasonAffix
+_table_.subAffix = subAffix
